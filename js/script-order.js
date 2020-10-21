7687e2bf71add8daf84cd35b599ba730c99ac342
Jaloch glitch <jalochfelix@gmail.com>
2020-10-21 05:26:15 +0300

    add delivery action argument

--- js/script-order.js
+++ js/script-order.js
@@ -142,3 +142,13 @@ function order(){
         });
     }); 
     var arrayTotal = []; //global array used to store all total prices for each order
+
+    //Deliver
+    $(".deliver").click(function () {
+        $('.summary').slideUp();
+        $('#list').slideUp();
+        $('.summary').text("Provide location details").slideDown();
+        $('.deliver').hide(1000);
+        $('.delivernot').hide(1000);
+        $('.cdata-overlay').slideDown();
+    });
